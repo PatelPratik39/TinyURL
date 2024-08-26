547d869e625eb313914e1b8121b72e7a9e2d5669
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-26 15:33:55 -0500

    landing Page configured

--- URL-SHORTNER/src/components/header.jsx
+++ URL-SHORTNER/src/components/header.jsx
@@ -1,17 +1,51 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { LinkIcon, LogOut } from "lucide-react";
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = true;
   return (
     <nav className="py-4 flex justify-between items-center">
       <Link to="/">
         <img src="/public/tinyUrl.png" className="h-16" alt="logo" />
       </Link>
       <div>
-        <Button onClick={() => navigate("/auth")}>Login</Button>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>PP</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Prats Patel</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                <span>My Links</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </nav>
   );
--- URL-SHORTNER/src/pages/Landing.jsx
+++ URL-SHORTNER/src/pages/Landing.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React,{useState} from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from "@/components/ui/accordion";
 
 const Landing = () => {
-  return (
-    <div>Landing</div>
-  )
-}
+  const [longUrl, setLongUrl] = useState();
 
-export default Landing
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
+        The only URl Shortner <br /> You&rsquo;ll ever need!!
+      </h2>
+      <form className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
+        <Input
+          type="url"
+          value={longUrl}
+          placeholder="Enter Your Long Url"
+          className="h-full flex-1 py-4 px-4"
+          onChange={(e) => setLongUrl(e.target.value)}
+        />
+        <Button className="h-full" type="submit" variants="destructive">
+          Shorten!
+        </Button>
+      </form>
+      <img src="/banner.jpg" alt="banner" className="w-full my-11 md:px-11" />
+      <Accordion type="multiple" collapsible className="w-full md:px-11">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>How does the Url Shortner works??</AccordionTrigger>
+          <AccordionContent>
+            When you enter a long Url, Our System generates a shorter version of
+            that URL. This shortened URL redirects to the original long URL when
+            accessed.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            Do i need an Account to use the app?
+          </AccordionTrigger>
+          <AccordionContent>
+            Yes.Creating an acount allow you to manage your urls, view
+            analytics, and customize your short URLs.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            What analytics are available for my shortened URLs??
+          </AccordionTrigger>
+          <AccordionContent>
+            You can view the number of clicks, geological data of the clicks and
+            device types(mobile/desktop) for each of your shortened URLs.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default Landing;
