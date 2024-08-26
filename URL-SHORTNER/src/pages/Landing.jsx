313a5e3d625554a491d881bbbb4b91ab8125c9d5
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-26 16:02:27 -0500

    supabase user creation and apiAuth.js file configured

--- URL-SHORTNER/src/pages/Landing.jsx
+++ URL-SHORTNER/src/pages/Landing.jsx
@@ -1,26 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger
 } from "@/components/ui/accordion";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-  const [longUrl, setLongUrl] = useState();
+  const [longUrl, setLongUrl] = useState("");
+  const navigate = useNavigate();
+
+  const handleShortened = (e) => {
+    e.preventDefault();
+    if (longUrl) navigate(`/auth?createNew=${longUrl}`);
+  };
 
   return (
     <div className="flex flex-col items-center">
       <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
-        The only URl Shortner <br /> You&rsquo;ll ever need!!
+        The only URL Shortener <br /> You&rsquo;ll ever need!!
       </h2>
-      <form className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
+      <form
+        className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2"
+        onSubmit={handleShortened}
+      >
         <Input
           type="url"
           value={longUrl}
-          placeholder="Enter Your Long Url"
+          placeholder="Enter Your Long URL"
           className="h-full flex-1 py-4 px-4"
           onChange={(e) => setLongUrl(e.target.value)}
         />
@@ -29,31 +39,31 @@ const Landing = () => {
         </Button>
       </form>
       <img src="/banner.jpg" alt="banner" className="w-full my-11 md:px-11" />
-      <Accordion type="multiple" collapsible className="w-full md:px-11">
+      <Accordion type="multiple" collapsible="true" className="w-full md:px-11">
         <AccordionItem value="item-1">
-          <AccordionTrigger>How does the Url Shortner works??</AccordionTrigger>
+          <AccordionTrigger>How does the URL Shortener work?</AccordionTrigger>
           <AccordionContent>
-            When you enter a long Url, Our System generates a shorter version of
+            When you enter a long URL, our system generates a shorter version of
             that URL. This shortened URL redirects to the original long URL when
             accessed.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
           <AccordionTrigger>
-            Do i need an Account to use the app?
+            Do I need an account to use the app?
           </AccordionTrigger>
           <AccordionContent>
-            Yes.Creating an acount allow you to manage your urls, view
+            Yes. Creating an account allows you to manage your URLs, view
             analytics, and customize your short URLs.
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionTrigger>
-            What analytics are available for my shortened URLs??
+            What analytics are available for my shortened URLs?
           </AccordionTrigger>
           <AccordionContent>
-            You can view the number of clicks, geological data of the clicks and
-            device types(mobile/desktop) for each of your shortened URLs.
+            You can view the number of clicks, geographical data of the clicks,
+            and device types (mobile/desktop) for each of your shortened URLs.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
